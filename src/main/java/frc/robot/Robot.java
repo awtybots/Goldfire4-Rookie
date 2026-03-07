740e233c82a39bf33a1129aea86a0a99915e11f1
DavidCui5829 <27cuiz@awty.org>
2026-03-06 23:31:42 -0600

    put megatag2 back on

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -82,7 +82,7 @@ public class Robot extends LoggedRobot {
    */
   @Override
   public void robotInit() {
-    drivebase.useMegaTag2 = false;
+    drivebase.useMegaTag2 = true;
     // LimelightHelpers.SetIMUMode("limelight-bright", 1); // Seed internal IMU
     // LimelightHelpers.SetIMUMode("limelight-bleft", 1); // Seed internal IMU
     // LimelightHelpers.SetIMUMode("limelight-climber", 1); // Seed internal IMU
@@ -142,7 +142,7 @@ public class Robot extends LoggedRobot {
    */
   @Override
   public void disabledInit() {
-    drivebase.useMegaTag2 = false;
+    drivebase.useMegaTag2 = true;
     // LimelightHelpers.SetIMUMode("limelight-bleft", 1); // Seed internal IMU
     // LimelightHelpers.SetIMUMode("limelight-bright", 1); // Seed internal IMU
     // LimelightHelpers.SetIMUMode("limelight-climber", 1); // Seed internal IMU
