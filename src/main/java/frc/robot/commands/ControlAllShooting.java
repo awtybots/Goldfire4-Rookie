299409f60f79e5e41a4bf2aac47b75ddccb7ced7
xeldridge18 <xeldridge13@gmail.com>
2026-04-03 16:59:24 -0500

    Update at 'Fri Apr 03 16:59:24 CDT 2026'

--- src/main/java/frc/robot/commands/ControlAllShooting.java
+++ src/main/java/frc/robot/commands/ControlAllShooting.java
@@ -81,14 +81,14 @@ public class ControlAllShooting extends Command
     
     for (var entry : List.of(
       // Pair.of(Meters.of(1), RPM.of((1000))),
-                            Pair.of(Meters.of(2), RPM.of(1700)),
-                            Pair.of(Meters.of(2.5), RPM.of(1835)),
-                            Pair.of(Meters.of(3), RPM.of(1950)),
-                            Pair.of(Meters.of(3.5), RPM.of(2090)),
+                            Pair.of(Meters.of(2), RPM.of(-1460)),
+                            Pair.of(Meters.of(2.5), RPM.of(-1695)),
+                            Pair.of(Meters.of(3), RPM.of(-1810)),
+                            Pair.of(Meters.of(3.5), RPM.of(-1950)),
                             // Pair.of(Meters.of(3.5), RPM.of(2050),
-                            Pair.of(Meters.of(4), RPM.of(2220)),
-                            Pair.of(Meters.of(5.2048), RPM.of(2456.918)),
-                            Pair.of(Meters.of(6), RPM.of(2600))
+                            Pair.of(Meters.of(4), RPM.of(-2080)),
+                            Pair.of(Meters.of(5.2048), RPM.of(-2317)),
+                            Pair.of(Meters.of(6), RPM.of(-2460))
                             
                             //  Pair.of(Meters.of(2), RPM.of(1700)),
                             // Pair.of(Meters.of(2.5), RPM.of(1815)),
