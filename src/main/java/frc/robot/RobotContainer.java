1fc05ec5f74829c50518d01832c4eb3925bc7713
xeldridge18 <xeldridge13@gmail.com>
2026-04-11 12:46:34 -0500

    Update at 'Sat Apr 11 12:46:34 CDT 2026'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -588,12 +588,12 @@ public class RobotContainer {
                 Commands.sequence(
                     Commands.waitUntil(() -> shootCmd.isCASAtSpeed()
                         && aimAtHub.swerveInputStream
-                            .aimLock(Angle.ofBaseUnits(aimTolerance(shootCmd.distance), Degrees)).getAsBoolean()
-                        && Math.abs(drivebase.getHubAimErrorDeg()) <= aimTolerance(shootCmd.distance)),
+                            .aimLock(Angle.ofBaseUnits(aimTolerance(5), Degrees)).getAsBoolean()
+                        && Math.abs(drivebase.getHubAimErrorDeg()) <= aimTolerance(5)),
                     Commands.parallel(
                         Commands.runOnce(() -> {
                           aimAtHub.readyToLock = true;
-                          Logger.recordOutput("Aim/DynamicAimLockTolerance", aimTolerance(shootCmd.distance));
+                          Logger.recordOutput("Aim/DynamicAimLockTolerance", aimTolerance(5));
                         }),
                         m_hopper.runHopperToShooterCommand(),
                         m_kicker.kickCommand(),
@@ -608,8 +608,8 @@ public class RobotContainer {
                               aimAtHub.readyToLock = false;
                             })
                         .onlyWhile(() -> aimAtHub.swerveInputStream
-                            .aimLock(Angle.ofBaseUnits(aimTolerance(shootCmd.distance), Degrees)).getAsBoolean()
-                            && Math.abs(drivebase.getHubAimErrorDeg()) <= aimTolerance(shootCmd.distance))));
+                            .aimLock(Angle.ofBaseUnits(aimTolerance(5), Degrees)).getAsBoolean()
+                            && Math.abs(drivebase.getHubAimErrorDeg()) <= aimTolerance(5))));
           } else {
             ControllAllPassing passCmd = makeVariablePass();
             return Commands.parallel(
