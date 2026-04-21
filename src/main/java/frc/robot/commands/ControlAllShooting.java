36aa6c6b6ef42d4316302d158ca44c2b5d44c01b
DavidCui5829 <27cuiz@awty.org>
2026-04-21 09:43:26 -0500

    ignored compiler problems (harmless)

--- src/main/java/frc/robot/commands/ControlAllShooting.java
+++ src/main/java/frc/robot/commands/ControlAllShooting.java
@@ -227,7 +227,7 @@ public class ControlAllShooting extends Command
   @Override
   public boolean isFinished()
   {
-    // TODO: Make this return true when this Command no longer needs to run execute()
+   
     return false;
   }
 
--- src/main/java/frc/robot/commands/ControllAllPassing.java
+++ src/main/java/frc/robot/commands/ControllAllPassing.java
@@ -207,7 +207,7 @@ public class ControllAllPassing extends Command
   @Override
   public boolean isFinished()
   {
-    // TODO: Make this return true when this Command no longer needs to run execute()
+   
     return false;
   }
 
