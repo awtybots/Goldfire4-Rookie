c3eeebcae130d1db0634af51dfb5bb44e5bdfe88
1adityatandon1234-netizen <27cuiz@awty.org>
2026-06-27 10:52:40 -0500

    Update at 'Sat Jun 27 10:52:39 CDT 2026'

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -158,8 +158,8 @@ public final class Constants {
 
     public static final double PUSHOUT_RETRACTED_POS = 2; //
     public static final double PUSHOUT_RIGHT_RETRACTED_POS = -2; //
-    public static final double PUSHOUT_EXTENDED_POS = 15.911; // TUNE THIS!!!
-    public static final double PUSHOUT_RIGHT_EXTENDED_POS = -15.911; // TUNE THIS!!!
+    public static final double PUSHOUT_EXTENDED_POS = 20.206;// 15.911; // TUNE THIS!!!
+    public static final double PUSHOUT_RIGHT_EXTENDED_POS = -20.206; // TUNE THIS!!!
     public static final double FULLY_RETRACTED_POS = 0.25;
     public static final double FULLY_RETRACTED_RIGHT_POS = -0.25;
 
