e06b98a021bf40338ac599ffd1c82b2193b98406
gummy-mbck <gummy.choco.donuts@gmail.com>
2026-08-30 16:22:31 -0500

    changed to operator controller

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -314,13 +314,13 @@ public class RobotContainer {
         .scaleTranslation(1.0)
         .allianceRelativeControl(true);
 
-dc().rightTrigger().whileTrue(
-    Commands.parallel(
-        m_kicker.kickCommand(),
-        m_shooter.setShooterSpeedCommand(1200),
-        m_hopper.runBeltsToConveyorCommand()
-    )
-);
+    oc().rightTrigger().whileTrue(
+        Commands.parallel(
+            m_kicker.kickCommand(),
+            m_shooter.setShooterSpeedCommand(1200),
+            m_hopper.runBeltsToConveyorCommand()
+        )
+    );
 
     dc().rightTrigger().whileTrue(Commands.defer(() -> {
       if (isInAllianceZone()) {
