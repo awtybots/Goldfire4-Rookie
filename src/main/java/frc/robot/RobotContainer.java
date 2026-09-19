36838bd7fda3a376110b3aed39a6398e9e4641fe
salamander <asiersg@icloud.com>
2026-09-19 14:00:46 -0500

    auchanfe

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -350,7 +350,7 @@ public class RobotContainer {
     dc().leftTrigger().whileTrue(
         Commands.parallel(
             m_slapdown.extendCommand(),
-            m_intake.runIntakeCommand().onlyIf(m_slapdown::isSlapdownOut)));
+            m_intake.runIntakeCommand().onlyWhile(m_slapdown::isSlapdownOut)));
 
     dc().leftBumper().whileTrue(
         (m_slapdown.retractCommand()));
