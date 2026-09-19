040ccda2a2ffef7cee09498a2ddb86d69d5472c7
DavidCui5829 <27cuiz@awty.org>
2026-09-19 15:27:48 -0500

    Update at 'Sat Sep 19 15:27:48 CDT 2026'

--- src/main/java/frc/robot/subsystems/Slapdown.java
+++ src/main/java/frc/robot/subsystems/Slapdown.java
@@ -62,7 +62,7 @@ public class Slapdown extends SubsystemBase {
     }
 
     public Boolean isSlapdownOut () {
-        return slapdownEncoder.getPosition() > 15;
+        return slapdownController.getSetpoint() > 15;
     }
     public Boolean isSlapdownIn () {
         return slapdownEncoder.getPosition() < 5;
