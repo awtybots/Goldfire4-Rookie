6a4cb08460feae55ab19c38b2118a8c2d97e6c5e
salamander <asiersg@icloud.com>
2026-09-19 09:45:51 -0500

    Update at 'Sat Sep 19 09:45:51 CDT 2026'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -350,7 +350,7 @@ public class RobotContainer {
     dc().leftTrigger().whileTrue(
         Commands.parallel(
             m_slapdown.extendCommand(),
-            m_intake.runIntakeCommand()));
+            m_intake.runIntakeCommand().beforeStarting(Commands.waitSeconds(0.5))));
 
     dc().leftBumper().whileTrue(
         (m_slapdown.retractCommand()));
