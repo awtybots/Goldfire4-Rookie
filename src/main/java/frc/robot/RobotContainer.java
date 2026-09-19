d38bff9bdb8066c2be4b7a7c3f5a98e791c7dd1c
DavidCui5829 <27cuiz@awty.org>
2026-09-19 15:30:49 -0500

    Update at 'Sat Sep 19 15:30:49 CDT 2026'

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -350,7 +350,7 @@ public class RobotContainer {
     dc().leftTrigger().whileTrue(
         Commands.parallel(
             m_slapdown.extendCommand(),
-            m_intake.runIntakeCommand().onlyWhile(m_slapdown::isSlapdownOut),
+            // m_intake.runIntakeCommand().onlyWhile(m_slapdown::isSlapdownOut),
             m_intake.runIntakeCommand().beforeStarting(Commands.waitSeconds(1.5)).onlyWhile(m_slapdown::isSlapdownIn)
             ));
 
